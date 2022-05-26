c9bfcd4b2f971fd6902a7707a480eb250b70c93f
nfrank5 <nfrankenfeld@gmail.com>
2022-05-26 15:19:39 -0300

    Add computerPlay function

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log(123)
+function computerPlay(){
+    let number = Math.floor(Math.random()*3+1)
+    switch(number){
+        case(1):{
+            return "Rock"
+        }
+        case(2):{
+            return "Paper"
+        }
+        case (3):{
+            return "Scissors"
+        }
+    }
+}
+
+console.log(computerPlay())
